6215f639c455891747f2e05d8befcb455ac15bba
Dana Franco <turquoise.ilg@hotmail.com>
2017-03-09 17:56:27 -0500

    avancé muy poco

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('load',function() {
+  var loginButton = document.getElementById('login');
+  loginButton.addEventListener('click',function() {
+    var emailInput = document.getElementById('email');
+    var passwordInput = document.getElementById('password');
+
+
+  });
+});
